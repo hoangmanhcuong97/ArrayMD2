e0a9e8d88677240c00e4e72bf1dfb8c46980ca34
hoangmanhcuong97 <hoangmanhcuong01997@gmail.com>
2021-07-15 13:45:32 +0700

    tinh tong duong cheo chinh

---
+++ src/addSumCheoChinh/AddSum.java
@@ -0,0 +1,38 @@
+package addSumCheoChinh;
+
+import java.util.Scanner;
+
+public class AddSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row:");
+        int row = sc.nextInt();
+        System.out.println("Enter col: ");
+        int col = sc.nextInt();
+        int[][] arr = new int[row][col];
+
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                System.out.println("Enter position "+ (1 + r) +""+ (1 + c)+":" );
+                arr[r][c] = sc.nextInt();
+            }
+        }
+        System.out.println("Ma tran moi co dang:");
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                System.out.print(arr[r][c] + "\t");
+            }
+            System.out.print("\n");
+        }
+        int sum = 0;
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                if( c == r){
+                    sum = sum + arr[r][c];
+                }
+            }
+
+        }
+        System.out.println("Tong cua duong cheo chinh la: " + sum);
+    }
+}
